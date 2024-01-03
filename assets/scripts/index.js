32a292257f8c2126104a302391a38f3c6300b91b
Yemata Ferry <yemataferry@gmail.com>
2024-01-03 17:17:55 +0100

    linter errors on js ajusted

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,44 +1,43 @@
-const mainBox = document.getElementById("container");
-const result = document.getElementById("result");
+const mainBox = document.getElementById('container')
+const result = document.getElementById('result')
 
-const refresh = document.createElement("button");
-const firstString = document.createElement("input");
-const secondString = document.createElement("input");
-const check = document.createElement("button");
-
+const refresh = document.createElement('button')
+const firstString = document.createElement('input')
+const secondString = document.createElement('input')
+const check = document.createElement('button')
 
 refresh.innerHTML = `<span class="material-symbols-outlined" style='color: white; padding: 5px;'>refresh</span>`
-firstString.placeholder = "Enter the first string";
-secondString.placeholder = "Enter the second string";
+firstString.placeholder = 'Enter the first string'
+secondString.placeholder = 'Enter the second string'
 check.innerHTML = `<p style='padding: 1rem;'>Check</p>`
 
 mainBox.appendChild(refresh)
-mainBox.appendChild(firstString);
-mainBox.appendChild(secondString);
+mainBox.appendChild(firstString)
+mainBox.appendChild(secondString)
 mainBox.appendChild(check)
 
 check.addEventListener('click', checkIsomorphic)
 
-function isomorphic(str1, str2) {
+function isomorphic (str1, str2) {
   for (let i = 0; i < str1.length; i++) {
-    const x = str1.indexOf(str1[i]);
-    const y = str2.indexOf(str2[i]);
+    const x = str1.indexOf(str1[i])
+    const y = str2.indexOf(str2[i])
     if (str2[x] !== str2[i] || str1[y] !== str1[i]) {
-      return false;
+      return false
     }
   }
-  return true;
+  return true
 }
 
-function checkIsomorphic() {
+function checkIsomorphic () {
   const string1 = firstString.value
   const string2 = secondString.value
   const answer = isomorphic(string1, string2)
 
-  if (string1 == '' && string2 == '') {
+  if (string1 === '' && string2 === '') {
     result.innerHTML += `<span class="material-symbols-outlined" style="color: red; font-size: 1.5rem;">error</span>` + 'No word has been entered, please try again!'
   } else if (string1.length !== string2.length) {
-    result.innerHTML += `<span class="material-symbols-outlined" style="color: red; font-size: 1.5rem;">error</span>` + 'The length of your strings should be the same';
+    result.innerHTML += `<span class="material-symbols-outlined" style="color: red; font-size: 1.5rem;">error</span>` + 'The length of your strings should be the same'
   } else if (answer) {
     result.innerHTML += 'The word entered is isomorphic'
   } else {
